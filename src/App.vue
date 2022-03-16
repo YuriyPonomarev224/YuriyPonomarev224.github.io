b8d828ca8ae704a975837baa1420cba13345f325
YuriyPonomarev <yuriy.ponomarev224@gmail.com>
2022-03-17 00:07:56 +0600

    123
    123

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <transition name="fade">
+    <router-view/>
+  </transition>
+</template>
+
+<style lang="scss">
+
+// { id: 9, image: 'tricerax.png', title: "Трицератопс", subTitle: "", firstText: "", secondText: ""}
+#app {
+  font-family: Open Sans, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+}
+</style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,70 @@
+<template>
+  <vueper-slides
+  class="no-shadow"
+  :visible-slides="3"
+  slide-multiple
+  :gap="3"
+  :slide-ratio="1 / 4"
+  :dragging-distance="200"
+  :breakpoints="{ 800: { visibleSlides: 2, slideMultiple: 2 } }">
+  <vueper-slide v-for="(slide, i) in slides" :key="i" :title="slide.title" :content="slide.content" :image="slide.image" :href="slide.content" />
+</vueper-slides>
+</template>
+
+<script>
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+export default {
+    name:"SliderMain",
+    components: { VueperSlides, VueperSlide },
+    data: () => ({
+        slides: [
+            {
+            title: '1',
+            content: 'ad',
+            image: require('../assets/img/Item_1.png') ,
+            link: "https://google.com"
+            },
+            {
+            title: '2',
+            content: 'href="https://google.com"',
+            image: require('../assets/img/Item_2.png') 
+            },
+            {
+            title: '3',
+            content: 'ALOE',
+            image: require('../assets/img/Item_3.png') 
+            },
+            {
+            title: 'Slide #1',
+            content: 'ALOE'
+            },
+            {
+            title: 'Slide #1',
+            content: 'ALOE'
+            },
+            {
+            title: 'Slide #1',
+            content: 'ALOE'
+            }
+        ]
+    })
+}
+</script>
+
+<style lang="scss">
+.vueperslides {
+    max-width: 90%;
+    margin: 0 auto;
+    height: 490px;
+    &__bullets {
+        display: none;
+    }
+}
+.vueperslide {
+    &__title {
+        position: relative;
+        top: 200px;
+    }
+}
+</style>
